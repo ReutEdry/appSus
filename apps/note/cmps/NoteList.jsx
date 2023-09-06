1cea740d8bf621a4d90f132677d1b511415a08dd
reut <reutedry1@gmail.com>
2023-09-06 19:43:21 +0300

    setting deleting todos

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -3,12 +3,21 @@ import { DynamicNote } from "./DynamicNote.jsx"
 
 export function NoteList({ notes }) {
 
+    function onDeleteNote(noteId) {
+        console.log(noteId)
+    }
+
     return (
         <section>
             <h1>note list</h1>
             {
                 notes.map(note => <section key={note.id}>
                     <DynamicNote id={note.id} type={note.type} info={note.info} />
+                    <button onClick={() => onDeleteNote(note.id)}>delete</button>
+                    <button>update</button>
+                    <button>bgc color</button>
+                    <button>pin</button>
+                    <button>send to email</button>
                 </section>
                 )
             }
--- apps/note/cmps/TxtNote.jsx
+++ apps/note/cmps/TxtNote.jsx
@@ -2,11 +2,7 @@ export function TxtNote({ info }) {
     return (
         <article className="noteTxtP preview">
             <h2>{info.txt}</h2>
-            <button>delete</button>
-            <button>update</button>
-            <button>bgc color</button>
-            <button>pin</button>
-            <button>send to email</button>
+
         </article>
     )
 }
