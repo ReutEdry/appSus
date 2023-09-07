c023d41d0fc08874240a755571cae176f00e324a
SaharMach <saharmach28@gmail.com>
2023-09-07 21:09:54 +0300

    mail css

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -7,18 +7,17 @@ import { mailService } from "../services/mail.service.js";
 
 
 export function MailPreview({mail}) {
-    
     const fullDate = utilService.formatDate(mail.sentAt)
 
-    
-function handleMailClick(mailId,val){
+
+    function handleMailClick(mailId,val){
         mailService.markAsRead(mailId,val)
             .then(mail => console.log(mail))
     }
     
 
     return (
-        <Link to={`/mail/${mail.id}`}>
+        <Link className="mail-a-link" to={`/mail/${mail.id}`}>
             <section className={`email-item ${mail.isRead ? 'read' : ''}`} onClick={() => handleMailClick(mail.id,!mail.isRead) }>
                 <div className="email-from">{mail.from}</div>
                 <div className="email-subject">{mail.subject}</div>
